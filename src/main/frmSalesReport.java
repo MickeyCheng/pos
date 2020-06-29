0f173e9ca2c18f8d18c242bd33c2505eb3173549
Joefrey Bartolome <joefrey_bartolome@optum.com>
2020-06-28 23:16:40 -0500

    Adding Sales Report Form

--- src/main/frmSalesReport.java
+++ src/main/frmSalesReport.java
@@ -22,7 +22,7 @@ import net.sf.jasperreports.engine.design.JasperDesign;
 import net.sf.jasperreports.engine.xml.JRXmlLoader;
 import net.sf.jasperreports.view.JasperViewer;
 
-public class frmReceiptTable extends javax.swing.JFrame {
+public class frmSalesReport extends javax.swing.JFrame {
 ResultSet rs;
 Connection conn;
 PreparedStatement pstmt;
@@ -30,7 +30,7 @@ String getTransactionId;
 DecimalFormat df = new DecimalFormat("#.00");
 SimpleDateFormat tdf = new SimpleDateFormat("dd/M/YYYY");
 
-    public frmReceiptTable() {
+    public frmSalesReport() {
         initComponents();
         doConnect();
         fillTable();
@@ -297,20 +297,21 @@ private void sortTable(){
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(frmReceiptTable.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmSalesReport.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(frmReceiptTable.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmSalesReport.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(frmReceiptTable.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmSalesReport.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(frmReceiptTable.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmSalesReport.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new frmReceiptTable().setVisible(true);
+                new frmSalesReport().setVisible(true);
             }
         });
     }
